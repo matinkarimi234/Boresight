65871c2c195e02d4577989e1c317687744fcfabc
TIR <matinkarimi234@gmail.com>
2025-08-07 18:43:32 +0330

    edit to match python 3.9

--- Boresight_Camera.py
+++ Boresight_Camera.py
@@ -14,17 +14,18 @@ ok_button_press_duration = 0
 def buttons_state_update_callback(flag):
     global ok_button_press_duration, ok_button_press_start_time
     """Callback to receive flags from ButtonControl."""
-    match flag:
-        case ButtonControl.OK_PRESSED:
-            # Start a timer when the button is pressed
-            ok_button_press_start_time = time.time()
-            print("OK button pressed")
-        case ButtonControl.OK_RELEASED:
-            print("OK button released")
-            if ok_button_press_start_time:
-                ok_button_press_duration = time.time() - ok_button_press_start_time
+    
+    if flag == ButtonControl.OK_PRESSED:
+        # Start a timer when the button is pressed
+        ok_button_press_start_time = time.time()
+        print("OK button pressed")
+        
+    elif flag == ButtonControl.OK_RELEASED:
+        print("OK button released")
+        if ok_button_press_start_time:
+            ok_button_press_duration = time.time() - ok_button_press_start_time
 
-                ok_button_press_start_time = None  # Reset the timer after release
+            ok_button_press_start_time = None  # Reset the timer after release
 
 def main():
     global ok_button_press_duration
@@ -58,27 +59,26 @@ def main():
             # Here we can add actions based on the state
             current_state = state_machine.get_state()
 
-            match current_state:
-                case StateMachineEnum.START_UP_STATE:
-                    pass
+            if current_state == StateMachineEnum.START_UP_STATE:
+                state_machine.change_state(StateMachineEnum.NORMAL_STATE)
 
-                case StateMachineEnum.NORMAL_STATE:
-                    if ok_button_press_duration >= 3: # 3 Seconds Pressed
+            elif current_state == StateMachineEnum.NORMAL_STATE:
+                if ok_button_press_duration >= 3: # 3 Seconds Pressed
                         ok_button_press_duration = 0
                         state_machine.change_state(StateMachineEnum.HORIZONTAL_ADJUSTMENT)
                         buzzer_control.toggle_buzzer(2,1,1)
 
-                case StateMachineEnum.RECORD_STATE:
-                    pass
+            elif current_state == StateMachineEnum.RECORD_STATE:
+                pass
 
-                case StateMachineEnum.HORIZONTAL_ADJUSTMENT:
-                    led_control.start_toggle(1, 1)
-                    print("Adjusting horizontally...")
-                    
-                case StateMachineEnum.VERTICAL_ADJUSTMENT:
-                    pass
-                case StateMachineEnum.SAVING_VIDEO_STATE:
-                    pass
+            elif current_state == StateMachineEnum.HORIZONTAL_ADJUSTMENT:
+                led_control.start_toggle(1, 1)
+                print("Adjusting horizontally...")
+
+            elif current_state == StateMachineEnum.VERTICAL_ADJUSTMENT:
+                pass
+            elif current_state == StateMachineEnum.SAVING_VIDEO_STATE:
+                pass
             time.sleep(0.125)
 
     # Start the state machine thread
